980e7bd37ecb83415753eb7117899ee857a4dcd1
Lathika4455 <144444262+Lathika4455@users.noreply.github.com>
2023-09-21 16:21:06 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,9 @@
+English = 10 
+Hindi = 9.9 
+Maths = 10
+Science =9.9;  
+SocialStudy = 10
+CGPA = (10+9.9+10+9.9+10)/(5.0)  
+CGPAper = 9.5 * (CGPA)  
+print(" CGPA percentage is:");  
+print(CGPAper);
